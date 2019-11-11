b552c202aa2cb1d1cbe13c9da19898a520b87fe1
Himank17Gupta <hemankgupta@gmail.com>
2019-11-11 17:40:56 +0530

    played with dependencies

--- src/components/PollVote.js
+++ src/components/PollVote.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import UserHeader from './userHeader';
+import Box from '@material-ui/core/Box';
+//import { MDBProgress } from 'mdbreact';
 export default function PollVote(props){
     console.log(props);
     var pollid=props.location.state.poll;
@@ -14,6 +16,18 @@ console.log(demopoll);
         <div><UserHeader user={userid}/>
         <br/>
         <h3>poll vote component {pollid} </h3>
+        <Box component="div" display="block" p={1} m={1} color="text.secondary" bgcolor="background.paper">
+        {demopoll.Question} ?
+      </Box>
+      <Box component="div" display="block" p={1} m={1} color="text.secondary" bgcolor="background.paper">
+        {demopoll.Description} .
+      </Box>
+      {/* <MDBProgress value={34} className="my-2" />
+      <MDBProgress material value={25} height="20px">
+        25%
+      </MDBProgress> */}
+
+
 
         </div>
         </Container>
