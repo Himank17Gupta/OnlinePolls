a18ca0d09044924d62a8e7337a0f287ea53f6323
Himank17Gupta <hemankgupta@gmail.com>
2019-11-12 14:02:07 +0530

    static components almost done

--- src/components/PollVote.js
+++ src/components/PollVote.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React, {useState} from 'react';
+import { Container, Button } from '@material-ui/core';
 import UserHeader from './userHeader';
 import Box from '@material-ui/core/Box';
 import ProgressBar from './dummyformdb';
+import { makeStyles } from '@material-ui/styles';
 //import { MDBProgress } from 'mdbreact';
 
-  
-  
 
-
-
+const classes = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
 export default function PollVote(props){
     console.log(props);
     var pollid=props.location.state.poll;
     var userid=props.location.state.user.user;
-var demopoll={Question:'dummyq',Description:'desc',Options:[ {option:"x",votes:5},{option:"y",votes:0},{option:"z",votes:2}],Date:''}
+var demopoll={Question:'dummy question for testing',Description:'dummy description for testing purposes elongated',
+              Options:[ {option:"Option1",votes:5},{option:"Option2",votes:0},{option:"Option3",votes:3}],Date:''}
+
+var [selectedOption,selectOption]=useState(null);
+
+//handleOptionSelect doesn't change the poll component objects and vote digits but do it only static way.
+function handleOptionSelect(opid){
+  console.log(opid);
+  selectOption(selectedOption=opid);
+}
+//handleOptionSelect doesn't change the poll component objects and vote digits but do it only static way.
+
+function handleVoteSubmit(){
+  console.log(selectedOption +"Rest Api Call with Axios");
+}
+
+
+console.log(selectedOption);
 console.log(demopoll);
 var totalvotes=0;
 demopoll.Options.forEach(obj=>{totalvotes+=obj.votes});
 console.log(totalvotes);
+if(selectedOption){totalvotes+=1;console.log(totalvotes);}
 
     return(
         <Container maxWidth='md'>
@@ -28,7 +51,7 @@ console.log(totalvotes);
         <Box component="div" display="block" p={1} m={1} color="text.secondary" bgcolor="background.paper">
         {demopoll.Question} ?
       </Box>
-      <Box component="div" display="block" p={1} m={1} color="text.secondary" bgcolor="background.paper">
+      <Box component="div" display="block" p={1} m={1} color="text.secondary" bgcolor="background.paper"  >
         {demopoll.Description} .
       </Box>
       </div>
@@ -37,9 +60,16 @@ console.log(totalvotes);
           <Box component="div" display="block" p={0.5} m={0.5} color="text.secondary" bgcolor="background.paper">
           {oObj.option} 
         </Box>
-<ProgressBar option ={oObj.option} votes={oObj.votes} totalvotes={totalvotes}  />
+<ProgressBar option ={oObj.option} votes={oObj.votes} totalvotes={totalvotes} selected={selectedOption} handleOptionSelect={handleOptionSelect.bind(this)} />
 </>
       )})}
+
+      <br/>
+      <br/>
+{selectedOption&&<Button variant="contained" color="secondary" style={{backgroundColor:'#b5315d'}} 
+className={classes.button} onClick={handleVoteSubmit}    >
+        Vote
+      </Button>} 
 
         </Container>
     )
--- src/components/dummyformdb.js
+++ src/components/dummyformdb.js
@@ -2,17 +2,26 @@ import React from 'react';
 
 function ProgressBar(props){
   console.log(props);
-  var votepercentage=(props.votes/props.totalvotes);
+  var votes=props.votes;
+  var tvotes=props.totalvotes;
+  var color='#6cadeb';
+  if(props.selected==props.option){votes+=1;color='#b5315d'}
+    //'#c971a5'}
+  var votepercentage=(votes/tvotes);
   votepercentage*=100;
  votepercentage= votepercentage.toFixed(2);
+
     return(
 
-      <div id="myProgress"  style={{ width: '100%',backgroundColor:'#ddd', borderRadius:'22px', cursor:'pointer'}}  onClick={()=>{console.log(props.option)}} >
+      <div id="myProgress"  style={{ width: '100%',backgroundColor:'#ddd', borderRadius:'22px', cursor:'pointer'}}  
+      onClick={()=>{console.log(props.option);
+      props.handleOptionSelect(props.option);
+      }} >
    <div id="myBar" 
    style={{
     width: `${votepercentage}%`,
     height: '30px',
-    backgroundColor: '#6cadeb',
+    backgroundColor: color,
     //'#eb4034',
     //'#4CAF50',
     textAlign: 'center',
