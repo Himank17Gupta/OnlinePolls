c5d3d7b784dbe1ec7abfeecd73fc3562addf7771
Himank17Gupta <hemankgupta@gmail.com>
2019-11-22 21:04:21 +0530

    added linear progress

--- src/components/pollList.js
+++ src/components/pollList.js
@@ -2,12 +2,14 @@ import React from 'react';
 import VoteCard from './voteCard';
 import { Container, GridList, GridListTile } from '@material-ui/core';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
 class PollList extends React.Component{
   constructor(props){
    super(props);
     this.nums=['a','b','c','d','e'];
     this.polls=[];
-    this.state={pollArray:[]};
+    this.state={pollArray:[],progress:true};
     this.prp=props;
     console.log(props);
   }
@@ -17,7 +19,7 @@ componentDidMount(){
 axios.post("https://pollsmernrestapi.herokuapp.com/getListofPolls",{}).then((res)=>{
   console.log(res);
   this.polls=res.data.filter(a=>a);
-this.setState({...this.state,pollArray:this.polls});
+this.setState({...this.state,pollArray:this.polls,progress:false});
   //  console.log(state.pollArray)
 console.log(this.state.pollArray);
 }).catch(err=>console.log(err))
@@ -35,8 +37,12 @@ if(window.innerWidth<500){columns=1}
 else columns=3;
     return(
     <div>
-        
-    polllists here ->..loader..->}<br/>
+       { this.state.progress?(<div>
+        <LinearProgress  color="secondary" />
+        </div>):(<></>)}
+      
+    
+    <br/>
 
     <GridList cols={columns} spacing={25} >
       {/* {this.nums.map(n => {
